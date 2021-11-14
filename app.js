07242f55a56998452ec299ad518104740de341a0
TrevorBeltmont <72894302+TrevorBeltmont@users.noreply.github.com>
2021-11-14 13:03:05 -0600

    Added testing page

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 $(document).ready(function done() {
   $.ajax({
-    url: 'https://api.thingspeak.com/channels/1563378/fields/field1/last.txt?api_key=MLLL5QF64O9ZP959',
+    url: 'https://api.thingspeak.com/channels/1563378/fields/field1/last.txt?api_key=VWXGS6E0DLG8BYH4',
     type: 'GET',
     data: {
       format: 'text'
     },
     success: function(x) {
-      var caja = document.getElementById("box1");
+      var caja = document.getElementById("satus-1");
       console.log(x);
-      caja.style.backgroundColor = 'green';
-      // document.write(x);
-      // document.write('<br>');
-      // setTimeout(function(){
-      //   done();
-      // }, 10000);
+      if(x == 1){
+        caja.style.backgroundColor = 'green';
+      }
+      else if(x == 0){
+        caja.style.backgroundColor = 'red';
+      }
+      setTimeout(function(){
+        done();
+      }, 10000);
+    },
+    error: function() {
+      $('#errors').text("Error");
+    }
+  });
+});
+
+$(document).ready(function done() {
+  $.ajax({
+    url: 'https://api.thingspeak.com/channels/1563378/fields/field2/last.txt?api_key=VWXGS6E0DLG8BYH4',
+    type: 'GET',
+    data: {
+      format: 'text'
+    },
+    success: function(x) {
+      var caja = document.getElementById("satus-2");
+      console.log(x);
+      if(x == 1){
+        caja.style.backgroundColor = 'green';
+      }
+      else if(x == 0){
+        caja.style.backgroundColor = 'red';
+      }
+      setTimeout(function(){
+        done();
+      }, 10000);
     },
     error: function() {
       $('#errors').text("Error");
